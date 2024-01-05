b8d812c49bc658258e1b41e82be4fbb2f861a997
Haze10425 <jheee042500@gmail.com>
2024-01-05 18:52:59 +0900

    feat: how to play 모달창 추가

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,20 +1,61 @@
 import question from '../../style/images/icon/question.svg';
 import character from '../../style/images/visual/char.png';
 import logo from '../../style/images/logo.png';
+import Carousel from '../../components/Carousel';
+import { useState } from 'react';
+
+type characterProps = {
+  id: number;
+  image_link: string;
+};
 
 const Main = () => {
+  const [isToggled, setIsToggled] = useState<boolean>(false);
+
+  const characterList: characterProps[] = [
+    { id: 1, image_link: character },
+    { id: 2, image_link: character },
+    { id: 3, image_link: character },
+    { id: 4, image_link: character },
+  ];
+
   return (
     <div className="page main">
       <h1>
-        <a href="javascript:;">미스터리 스케치</a>
         <img src={logo} alt="미스터리 스케치" className="title" />
       </h1>
+      {isToggled && (
+        <div className="howToPlayArea" onClick={() => setIsToggled(false)}>
+          <div className="playContainer">
+            <h2>How to Play</h2>
+            <h3>MYSTERY SKETCH에 오신 걸 환영합니다!</h3>
+            <div className="explanation">
+              <ul>
+                <li>
+                  각 라운드마다 플레이어들은 무작위로 할당된 단어를 그려 다른
+                  플레이어들이 추측할 수 있도록 합니다.
+                </li>
+                <li>
+                  플레이어는 채팅을 통해 정답을 맞추면 해당 라운드가 종료되며
+                  다음 라운드가 진행됩니다.
+                </li>
+              </ul>
+              <p>
+                가장 많은 점수를 얻어 1등에 도전하세요!
+                <br />
+                게임을 즐기면서 즐거운 시간 보내세요!
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="selectContainer">
-        <div className="questionArea">
+        <div className="questionArea" onClick={() => setIsToggled(true)}>
           <img src={question} alt="questionIcon" className="questionIcon" />
         </div>
-        <div className="characterArea">
-          <img src={character} alt="캐릭터이미지" />
+        <div className="slideArea">
+          <div className="characterBg" />
+          <Carousel character={characterList} />
         </div>
         <input type="text" placeholder="enter your name" className="userName" />
         <div className="btnArea">
