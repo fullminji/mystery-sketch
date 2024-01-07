13009e560f3620d0b74484f406853135668e11c4
홍지영 <hjy961021@gmail.com>
2024-01-07 18:09:18 +0900

    fix : 구조 변경

--- src/pages/Room/Room.tsx
+++ src/pages/Room/Room.tsx
@@ -22,38 +22,10 @@ const Room = () => {
   return (
     <div className="page room">
       <div className="roomArea">
-        <div className="roomTop">
+        <div className="roomGroup">
           <h1 className="logo">
             <a href="javascript">로고</a>
           </h1>
-          <div className="quizArea">
-            <div className="timeArea">
-              <span className="time">90</span>
-            </div>
-            <div className="answerArea">
-              <div className="answer">포</div>
-              <div className="answer" />
-              <div className="answer" />
-              <div className="answer" />
-            </div>
-            <div className="btnArea">
-              <button type="button" className="btn">
-                포기
-              </button>
-            </div>
-          </div>
-          <div className="settingArea">
-            <button type="button" className="btn lockOpen lockClose">
-              <span>잠금</span>
-              <span>열림</span>
-            </button>
-            <button type="button" className="btn soundOPen soundClose">
-              <span>사운드 켜기</span>
-              <span>사운드 끄기</span>
-            </button>
-          </div>
-        </div>
-        <div className="roomBottom">
           <ul className="userArea">
             <li className="me">
               <div className="user">
@@ -102,146 +74,174 @@ const Room = () => {
               <span className="user no">NO USER</span>
             </li>
           </ul>
-          <div className="sketchArea">
-            <div className="canvasArea">
-              <canvas ref={canvasRef}>dfa</canvas>
+        </div>
+        <div className="roomGroup">
+          <div className="quizArea">
+            <div className="timeArea">
+              <span className="time">90</span>
             </div>
-            <div className="pencilArea">
-              <ul className="colorArea">
-                <li>
-                  <input
-                    type="radio"
-                    id="color01"
-                    name="colorGroup"
-                    className="formRadio"
-                  />
-                  <label htmlFor="color01" className="formLabel">
-                    <span>빨강</span>
-                  </label>
-                </li>
-                <li>
-                  <input
-                    type="radio"
-                    id="color02"
-                    name="colorGroup"
-                    className="formRadio"
-                  />
-                  <label htmlFor="color02" className="formLabel">
-                    <span>주황</span>
-                  </label>
-                </li>
-                <li>
-                  <input
-                    type="radio"
-                    id="color03"
-                    name="colorGroup"
-                    className="formRadio"
-                  />
-                  <label htmlFor="color03" className="formLabel">
-                    <span>노랑</span>
-                  </label>
-                </li>
-                <li>
-                  <input
-                    type="radio"
-                    id="color04"
-                    name="colorGroup"
-                    className="formRadio"
-                  />
-                  <label htmlFor="color04" className="formLabel">
-                    <span>초록</span>
-                  </label>
-                </li>
-                <li>
-                  <input
-                    type="radio"
-                    id="color05"
-                    name="colorGroup"
-                    className="formRadio"
-                  />
-                  <label htmlFor="color05" className="formLabel">
-                    <span>파랑</span>
-                  </label>
-                </li>
-                <li>
-                  <input
-                    type="radio"
-                    id="color06"
-                    name="colorGroup"
-                    className="formRadio"
-                  />
-                  <label htmlFor="color06" className="formLabel">
-                    <span>남색</span>
-                  </label>
-                </li>
-                <li>
-                  <input
-                    type="radio"
-                    id="color07"
-                    name="colorGroup"
-                    className="formRadio"
-                  />
-                  <label htmlFor="color07" className="formLabel">
-                    <span>보라</span>
-                  </label>
-                </li>
-                <li>
-                  <input
-                    type="radio"
-                    id="color08"
-                    name="colorGroup"
-                    className="formRadio"
-                  />
-                  <label htmlFor="color08" className="formLabel">
-                    <span>흰색</span>
-                  </label>
-                </li>
-                <li>
-                  <input
-                    type="radio"
-                    id="color09"
-                    name="colorGroup"
-                    className="formRadio"
-                    checked
-                  />
-                  <label htmlFor="color09" className="formLabel">
-                    <span>검정</span>
-                  </label>
-                </li>
-              </ul>
-              <ul className="drawArea">
-                <li>
-                  <input
-                    type="radio"
-                    id="draw01"
-                    name="drawGroup"
-                    className="formRadio"
-                    checked
-                  />
-                  <label htmlFor="draw01" className="formLabel">
-                    <span>연필</span>
-                  </label>
-                </li>
-                <li>
-                  <input
-                    type="radio"
-                    id="draw02"
-                    name="drawGroup"
-                    className="formRadio"
-                  />
-                  <label htmlFor="draw02" className="formLabel">
-                    <span>지우개</span>
-                  </label>
-                </li>
-                <li>
-                  <button type="button" className="btn">
-                    <span>전체 지우기</span>
-                  </button>
-                </li>
-              </ul>
+            <div className="answerArea">
+              <div className="answer">포</div>
+              <div className="answer" />
+              <div className="answer" />
+              <div className="answer" />
+            </div>
+            <div className="btnArea">
+              <button type="button" className="btn">
+                포기
+              </button>
             </div>
           </div>
-          <Chat />
+          <div className="canvasArea">
+            <canvas ref={canvasRef}>dfa</canvas>
+          </div>
+          <div className="pencilArea">
+            <ul className="colorArea">
+              <li>
+                <input
+                  type="radio"
+                  id="color01"
+                  name="colorGroup"
+                  className="formRadio"
+                />
+                <label htmlFor="color01" className="formLabel">
+                  <span>빨강</span>
+                </label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  id="color02"
+                  name="colorGroup"
+                  className="formRadio"
+                />
+                <label htmlFor="color02" className="formLabel">
+                  <span>주황</span>
+                </label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  id="color03"
+                  name="colorGroup"
+                  className="formRadio"
+                />
+                <label htmlFor="color03" className="formLabel">
+                  <span>노랑</span>
+                </label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  id="color04"
+                  name="colorGroup"
+                  className="formRadio"
+                />
+                <label htmlFor="color04" className="formLabel">
+                  <span>초록</span>
+                </label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  id="color05"
+                  name="colorGroup"
+                  className="formRadio"
+                />
+                <label htmlFor="color05" className="formLabel">
+                  <span>파랑</span>
+                </label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  id="color06"
+                  name="colorGroup"
+                  className="formRadio"
+                />
+                <label htmlFor="color06" className="formLabel">
+                  <span>남색</span>
+                </label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  id="color07"
+                  name="colorGroup"
+                  className="formRadio"
+                />
+                <label htmlFor="color07" className="formLabel">
+                  <span>보라</span>
+                </label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  id="color08"
+                  name="colorGroup"
+                  className="formRadio"
+                />
+                <label htmlFor="color08" className="formLabel">
+                  <span>흰색</span>
+                </label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  id="color09"
+                  name="colorGroup"
+                  className="formRadio"
+                />
+                <label htmlFor="color09" className="formLabel">
+                  <span>검정</span>
+                </label>
+              </li>
+            </ul>
+            <ul className="drawArea">
+              <li>
+                <input
+                  type="radio"
+                  id="draw01"
+                  name="drawGroup"
+                  className="formRadio"
+                />
+                <label htmlFor="draw01" className="formLabel">
+                  <span>연필</span>
+                </label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  id="draw02"
+                  name="drawGroup"
+                  className="formRadio"
+                />
+                <label htmlFor="draw02" className="formLabel">
+                  <span>지우개</span>
+                </label>
+              </li>
+              <li>
+                <button type="button" className="btn">
+                  <span>전체 지우기</span>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="roomGroup">
+          <div className="settingArea">
+            <button type="button" className="btn lockOpen lockClose">
+              <span>잠금</span>
+              <span>열림</span>
+            </button>
+            <button type="button" className="btn soundOPen soundClose">
+              <span>사운드 켜기</span>
+              <span>사운드 끄기</span>
+            </button>
+          </div>
+          <div className="chatArea">
+            <Chat />
+          </div>
         </div>
       </div>
     </div>
