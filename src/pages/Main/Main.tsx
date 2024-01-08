ee0d04cbb99c4fa1109bb859dbf95cbb68ce3aee
Haze10425 <jheee042500@gmail.com>
2024-01-08 23:14:45 +0900

    feat: how to play ui 수정

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -24,32 +24,33 @@ const Main = () => {
       <h1>
         <img src={logo} alt="미스터리 스케치" className="title" />
       </h1>
-      {isToggled && (
-        <div className="howToPlayArea" onClick={() => setIsToggled(false)}>
-          <div className="playContainer">
-            <h2>How to Play</h2>
-            <h3>MYSTERY SKETCH에 오신 걸 환영합니다!</h3>
-            <div className="explanation">
-              <ul>
-                <li>
-                  각 라운드마다 플레이어들은 무작위로 할당된 단어를 그려 다른
-                  플레이어들이 추측할 수 있도록 합니다.
-                </li>
-                <li>
-                  플레이어는 채팅을 통해 정답을 맞추면 해당 라운드가 종료되며
-                  다음 라운드가 진행됩니다.
-                </li>
-              </ul>
-              <p>
-                가장 많은 점수를 얻어 1등에 도전하세요!
-                <br />
-                게임을 즐기면서 즐거운 시간 보내세요!
-              </p>
+
+      <div className="selectContainer" ref={selectRef}>
+        {isToggled && (
+          <div className="howToPlayArea" onClick={() => setIsToggled(false)}>
+            <div className="playContainer">
+              <h2>How to Play</h2>
+              <h3>MYSTERY SKETCH에 오신 걸 환영합니다!</h3>
+              <div className="explanation">
+                <ul>
+                  <li>
+                    각 라운드마다 플레이어들은 무작위로 할당된 단어를 그려 다른
+                    플레이어들이 추측할 수 있도록 합니다.
+                  </li>
+                  <li>
+                    플레이어는 채팅을 통해 정답을 맞추면 해당 라운드가 종료되며
+                    다음 라운드가 진행됩니다.
+                  </li>
+                </ul>
+                <p>
+                  가장 많은 점수를 얻어 1등에 도전하세요!
+                  <br />
+                  게임을 즐기면서 즐거운 시간 보내세요!
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      )}
-      <div className="selectContainer">
+        )}
         <div className="questionArea" onClick={() => setIsToggled(true)}>
           <img src={question} alt="questionIcon" className="questionIcon" />
         </div>
